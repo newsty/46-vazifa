537acd421c381afd6100cf79ab0be80e51cd8b8c
newsty <xadichaermatova19@gmail.com>
2023-08-09 10:23:38 +0500

    rest

---
+++ main.js
@@ -0,0 +1,87 @@
+// // 1-masala
+// // rest va spead
+// const [...num]=[1,2,3];
+// const [...str]=['bir','ikki','uch'];
+// const combine=[...num,...str];
+// console.log(combine);
+
+
+
+// // 2-masala
+// const {...obj1}={name:'Rano',age:21};
+// const {...obj2}={city:"O'zbekiston",gender:'women'};
+// const newObj={...obj1,...obj2}
+// console.log(newObj);
+
+
+// // 3-masala
+// // rest
+// const [...numbers1]=[56,12,87];
+// const {...obj}={name:'Solixa',age:4}
+// const com=[[...numbers1],{...obj}]
+// console.log(...com);
+
+
+// // 4-masala
+// const [number1,number2,...numbers2]=[45,76,45,52,465,234];
+// const [str1,str2,...str3]=['hello','rest','spread'];
+// const combine2=[number1,number2,...numbers2,str1,str2,...str3];
+// console.log(combine2);
+
+// 5-masala
+// const [arr1,arr2,...array]=['arr1','arr2',14,16,18];
+// const {...obj3}={name:'olma',kg:5,data:'08.08.2023',sum:156000};
+// const [...arrObj]=[[arr1,arr2,...array],{...obj3}]
+// console.log(...arrObj);
+
+// 1-masala
+// recursion
+// function myFun(num){
+//     if(num===8){
+//         return;
+//     }
+//     console.log(num);
+//     myFun(num+1)
+// }
+// myFun(4)
+// 2-masala
+// function x(number){
+//     if(number ===5 ){
+//    return;
+//     }
+//     console.log(number);
+//     x(number+1)
+// }
+// x(1)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
